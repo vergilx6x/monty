a0100be4b2b4c745abf2934af9e2911ff58973e7
vergilx6x <med.work96@hotmail.com>
2024-03-28 13:36:55 -0700

    fix

--- main.c
+++ main.c
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
 	file = fopen(argv[1], "r");
 	if (!file)
 	{
-		fprintf(stderr, "Error Can't open file %s\n", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	args.file = file;
