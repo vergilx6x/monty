d9562db6738e6f75ba1cfee1a2d45f0d52be738b
vergilx6x <med.work96@hotmail.com>
2024-03-28 08:49:50 -0700

    Task_1

--- pint.c
+++ pint.c
@@ -10,8 +10,8 @@ void _pint(stack_t **head, unsigned int counter)
 	if (*head == NULL)
 	{
 		fprintf(stderr, "L%u: can't pint, stack empty\n", counter);
-		fclose(bus.file);
-		free(bus.content);
+		fclose(args.file);
+		free(args.buffer);
 		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
